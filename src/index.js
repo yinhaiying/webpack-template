cd25cbd13cf91eabcf87f163048ac331ff7ddb76
yinhaiying <yinhaiying@58ganji-corp.com>
2019-10-22 11:39:59 +0800

    '添加index.html文件并引入打包后的js'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log('hello world');
+document.getElementById('app').innerHTML = "从零开始搭建webpack";
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
+    mode:"development",
     entry:'./src/index.js',
     output:{
-        path:path.join(__dirname,'dist'),
+        path:path.resolve(__dirname,'dist'),
         filename:'bundle.js'
     }
 }
