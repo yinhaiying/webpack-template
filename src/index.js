781c24679588a12e21bd6d21a44f16c72170fe76
yinhaiying <yinhaiying@58ganji-corp.com>
2019-10-28 18:02:19 +0800

    '使用babel处理ES6,ES7代码'

--- src/index.js
+++ src/index.js
@@ -13,3 +13,15 @@ import src from '../assets/images/1.jpg';
 let image = new Image();
 image.src = src;
 document.body.appendChild(image);
+
+let obj = {
+    a:'1',
+    b:'2'
+}
+
+let foo = ({a,b}) => {
+    console.log(a);
+    console.log(b);
+}
+
+foo(obj)
